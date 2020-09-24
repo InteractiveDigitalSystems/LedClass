c2248408bbc539ef613b2c5b8205659a507803dd
EbbeVang <ev@evang.dk>
2020-09-24 12:44:46 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <Led.h>
+
+int ledPin = 23;
+Led led(ledPin);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  led.
+  led.on();
+  Serial.println(led.status());
+  delay(1000);
+  led.off();
+  Serial.println(led.status());
+  delay(1000);
+}
